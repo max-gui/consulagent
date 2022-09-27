abc3305e3ad01f1d362111e59c8cdbbed3335798
max <playstation.gui@gmail.com>
2022-09-27 18:52:35 +0800

    remove info

--- pkg/consulsets/consulsets.go
+++ pkg/consulsets/consulsets.go
@@ -18,9 +18,8 @@ func StartupInit(acltoken string) {
 
 func init() {
 	// Appname = "charon"
-	Acltoken = flag.String("acltoken", "13a3dbe0-199c-af04-ae9d-b4c43eb735eb", "consul acltoken") //245d0a09-7139-config-prod-ff170a0562b1
-
-	Consul_host = flag.String("consulhost", "http://consul-stg.paic.com.cn", "consul url") //http://consul-prod.kube.com http://consul-stg.kube.com http://consul-szf-prod.kube.com
+	Acltoken = flag.String("acltoken", "", "consul acltoken")
+	Consul_host = flag.String("consulhost", "", "consul url")
 	Cacheminutes = flag.Int("cacheminutes", 5, "service cache for minutes")
 }
 
